5be04444752f38c36a6f504e66748118d4607b5b
xnorx <thogordon@augusta.edu>
2020-07-07 01:22:20 -0400

    created helper function and type checking on args in evoke_api().

--- monophony/calls/caller.py
+++ monophony/calls/caller.py
@@ -6,12 +6,12 @@ import pp
 
 # Generate appropriate timespans.
 def timespan(s):
-    ''' Return a timedelta of 1 hours from now. '''
+    """ Return a timedelta of 1 hours from now. """
     return dt.utcnow() - timedelta(seconds=s)
 
 
 def isoformat(d):
-    ''' Take a delta t and output t as ISO 8601. '''
+    """ Take a delta t and output t as ISO 8601. """
     return d.replace(microsecond=0).isoformat() + 'Z'
 
 
@@ -20,45 +20,88 @@ def lasthour():
 
 
 # Query and Generate API payloads
+def isiterable(obj):
+  try:
+    iter(obj)
+  except Exception:
+    return False
+  else:
+    return True
+
 def member(a,b):
-    ''' Takes a value a & returns if a is an element of b. '''
+    """ Takes a value a & returns if a is an element of b. """
     if a in set(elem.value for elem in b):
         return a
 
 
 def evoke(m, o):
-    ''' Takes a string m and o, & returns a matching object in module m by name. '''
+    """ Takes a string m and o, & returns a matching object in module m by name. """
     return getattr(globals()[m], o)
 
 
 def evoke_api(m, f, a):
-    ''' Takes a function f of module m & returns f() if f is an api in m. '''
+    """ Takes a function f of module m & returns f() if f is an api in m. """
     if member(f, evoke(m, 'Api')):
+      if isiterable(a):
         return evoke(m, f)(*a)
+      else:
+        try:
+          return evoke_api(m, f, [a])
+        except:
+          raise TypeError('args a must be an iterable.')
+
 
 
 def buildurl(m, p, q):
-    ''' Take a root r and leafs l, q & return str(r/l/q) '''
+    """ Take a root r and leafs l, q & return str(r/l/q). """
     return '/'.join((evoke(m,'rooturl')(), p, q))
 
 
 def get(app, api, endpoint, args):
-    ''' Request takes a payload p for a given api and returns a get request. '''
+    """ Takes an app, api, and args (where needed). Returns a GET request. 
+
+    Parameters
+    ----------
+    app: str
+      The application name. Use app.<Name>.value if string is not known.
+    api: str
+      The api name for a given application.
+      Use <app>.Api.value if string is not known.
+    endpoint: str
+      The endpoint of a given api.
+      Use <app>.<Name>.<Endpoint>.value
+
+    """ 
     return requests.get(
         buildurl(app, api, endpoint),
         params=evoke_api(app, api, args),
         auth=(config.PP_KEY, config.PP_SEC)
-    ).text
+    ).text #TODO move .text and wrap in other function
 
 # Create Requests
 # Arguments can be passed in the form of tuples, dicts, or directly, etc...
 # Very Attacked People
 # TODO this will have a command structure...
+def helper(f, args):
+  if isinstance(args, list):
+      return f(*args)
+  elif isinstance(args, dict):
+      return f(**args)
+  else:
+    try:
+      #TODO should work with evoke(m,f)(a) when 
+      print('trying...')
+      return f(*list(args))
+    except:
+      print(type(args))
+      raise TypeError('Collection C must be an iterable.')
+
+# TODO with helper function and get, build common api commands.
 vap_args = {
             'app': 'pp',
             'api': 'people',
             'endpoint': 'vap',
-            'args': ()
+            'args': 90
             }
 
 # Clickes Blocked
@@ -68,8 +111,10 @@ cb_args = (app.PP.value, pp.Api.SIEM.value, siemendpoint.Clicksblocked.value, op
 # Messages Blocked
 mb_args = (app.PP.value, pp.Api.SIEM.value, siemendpoint.Messagesblocked.value, options)
 
-
+print(helper(get,vap_args))
 #print(get(**vap_args))
-print(get(*cb_args))
-print(get(*mb_args))
+#print(get(*cb_args))
+#print(get(*mb_args))
+print(pp.SIEM.Clicksblocked.value)
+
 
--- monophony/calls/pp.py
+++ monophony/calls/pp.py
@@ -7,40 +7,65 @@ import requests
 
 # Enumerate Proofpoint APIs and API calls.
 # Enumerators are used similar to Haskell records.
+# These may be removed for tuples or dicts...
 Api = Enum('Api',
-           {'SIEM':'siem',
-            'People':'people',
-            'Foresnics': 'forensics',
-            'Campaign': 'campaign'
-            }
-           )
+    {
+      'SIEM':'siem',
+      'People':'people',
+      'Foresnics': 'forensics',
+      'Campaign': 'campaign'
+      }
+    )
 
+
+SIEM = Enum('SIEM',
+    {
+      'Clicksblocked': 'clicks/blocked',
+      'Clickspermitted': 'clicks/permitted',
+      'Messagesblocked': 'messages/blocked',
+      'Messagesdelivered': 'messages/delivered',
+      'Issues': 'issues',
+      'Everything': 'all'
+      }
+    )
+
+#TODO Finish adding Enums for all API Endpoints.
 
 def rooturl():
     return 'https://tap-api-v2.proofpoint.com/v2'
 
 
 def siem(t,s):
-    ''' Returns a payload for the SIEM API. '''
+    """ Returns a payload for the SIEM API. 
+
+    Parameters
+    ----------
+
+    t: time duration in ISO 8601 format
+      To date, Proofpoint SIEM API only accepts 3600s (1 hour)...
+    s: str
+
+    """
+    #TODO list s options 
     return {
-                'format': 'json',
-                'sinceTime': t,
-                'threatStatus': s
-    }
+        'format': 'json',
+        'sinceTime': t,
+        'threatStatus': s
+        }
 
 
-def people():
-    ''' Returns a Payload for the people API. '''
-    return {'window': 90}
+def people(n):
+    """ Takes an n-many days, returns a Payload for the people API. """
+    return {'window': n}
 
 
 def forensics():
-    ''' Returns a Payload for the forensics API. '''
+    """ Returns a Payload for the forensics API. """
     return {'threatId': get_threatid() }
 
 
 def campaign():
-    ''' Returns a campaign ID. '''
+    """ Returns a campaign ID. """
     return get_campaignid()
 
 
