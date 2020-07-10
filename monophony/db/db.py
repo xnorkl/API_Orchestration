070b3fa307d2893bc2492a89ba7e22f1c675f368
Thomas Gordon <gordont@monhealthsys.org>
2020-07-10 10:03:28 -0400

    Added dp.py. Early build.

---
+++ monophony/db/db.py
@@ -0,0 +1,56 @@
+import sqlite3
+import json
+import sys
+import re
+
+sys.path.append("../calls")
+from caller import get
+
+def access(db='monophony.db'):
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    return conn, c
+
+def people(*args):
+    conn, c = access()
+
+    c.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='vap' ''')
+    if not c.fetchone()[0]==1:
+        c.execute("CREATE TABLE vap (guid TEXT , email TEXT, data JSON)")
+
+    m = get('pp','people','vap')
+    d = json.loads(m.text)
+    for i in d['users']:
+        c.execute("insert into vap values (?, ?, ?)",
+            [i['identity']['guid'], i['identity']['emails'][0], json.dumps(i)]
+        )
+        conn.commit()
+
+    conn.close()
+
+def siem(ep,*args):
+    conn, c = access()
+
+    c.execute(''' SELECT count(name) FROM sqlite_master Where type='table' AND name='{}' '''.format(ep))
+    if not c.fetchone()[0]==1:
+        c.execute("Create TABLE {} (messageID TXT, fromAddress TXT, data JSON)".format(ep))
+    endpoint = re.sub(r'(messages)|(clicks)', r'\1\2/', ep).lower()
+    m = get('pp', 'siem', endpoint)
+    d = json.loads(m.text)
+    for i in d[ep]:
+        c.execute("insert into {} values (?, ?, ?)".format(ep),
+            [ i['GUID'], ''.join(i['fromAddress']), json.dumps(i)]
+        )
+        conn.commit()
+    conn.close()
+
+def cycle():
+    conn, c = access()
+    conn.close()
+
+cycle()
+people()
+siem(ep='clicksBlocked')
+siem(ep='clicksPermitted')
+siem(ep='messagesBlocked')
+siem(ep='messagesDelivered')
